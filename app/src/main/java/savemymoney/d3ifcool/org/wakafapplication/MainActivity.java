43460886409f37f78a863c285e1e4ee8d99a8404
yoga afrizal riandika <yogaafrizal@gmail.com>
2020-03-10 13:21:42 +0700

    Initial commit

---
+++ app/src/main/java/savemymoney/d3ifcool/org/wakafapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package savemymoney.d3ifcool.org.wakafapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
